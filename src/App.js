ecd35b523389c8956ed2d06edd93f6a53dfaedf8
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-12-13 11:19:46 +0200

    create days component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Persons/Person';
+import Days from './components/Days/Days';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="schedule">
-
+            <Days/>
             
-          
-          {/* <div> */}
-
-            <div className="days">
-              <div className="name">Име</div>
-              <div className="day">Пон</div>
-              <div className="day">Вт</div>
-              <div className="day">Ср</div>
-              <div className="day">Четв</div>
-              <div className="day">Пт</div>
-              <div className="day">Съб</div>
-              <div className="day">Нед</div>
-            </div>
             <div className="names">
               <Person />
               <Person />
               <Person />
               <Person />
-             
-                {/* <div className="name">Миро</div>
-                <div className="name">Никол</div>
-                <div className="name">Марийка</div>
-                <div className="name">Дони</div>
-                <div className="shifts">
-                  <div className="shift">1</div>
-                  <div className="shift">2</div>
-                  <div className="shift">1</div>
-                  <div className="shift">2</div>
-                </div> */}
             </div>
-          {/* </div> */}
           </div>
         </div>
 
---
+++ src/components/Days/Days.js
@@ -0,0 +1,16 @@
+import classes from "./Days.module.css";
+const Days = () =>{
+    return (
+    <div className={classes.days}>
+        <div className="name">Име</div>
+        <div className={classes.day}>Пон</div>
+        <div className={classes.day}>Вт</div>
+        <div className={classes.day}>Ср</div>
+        <div className={classes.day}>Четв</div>
+        <div className={classes.day}>Пт</div>
+        <div className={classes.day}>Съб</div>
+        <div className={classes.day}>Нед</div>
+    </div>
+    )
+};
+export default Days;
